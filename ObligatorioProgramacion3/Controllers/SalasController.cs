2c6c5ec0904e4a6d5d29892ddb563f1bd41476c7
fbarbosaa <f.barbosa.suarez5@gmail.com>
2023-07-18 20:35:15 -0300

    crud peliculas,salas y empleados

---
+++ ObligatorioProgramacion3/Controllers/SalasController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObligatorioProgramacion3.Datos;
+using ObligatorioProgramacion3.Models;
+
+namespace ObligatorioProgramacion3.Controllers
+{
+    public class SalasController : Controller
+    {
+        public readonly ApplicationDbContext? _context;
+
+        public SalasController(ApplicationDbContext? context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Sala>? List = _context?.Salas?.ToList();
+            return View(List);
+        }
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Sala sala)
+        {
+            if (ModelState.IsValid)
+            {
+                _context?.Salas?.Add(sala);
+                _context?.SaveChanges();
+                // return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            var sala = _context?.Salas?.FirstOrDefault(c => c.Id == id);
+            return View(sala);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Sala sala)
+        {
+            if (ModelState.IsValid)
+            {
+                _context?.Salas?.Update(sala);
+                _context?.SaveChanges();
+                // return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sala);
+        }
+        [HttpGet]
+        public IActionResult Borrar(int? id)
+        {
+            var sala = _context?.Salas?.FirstOrDefault(c => c.Id == id);
+            if (sala != null)
+            {
+                _context?.Salas?.Remove(sala);
+            }
+            _context?.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
